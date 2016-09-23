25d68bd263a0fa8084e403b1411da3af80a050e6
jdokoop <orjuela.koop@gmail.com>
2016-09-22 22:01:30 -0600

    Made prediction function reactive

--- TextPredictionApp/server.R
+++ TextPredictionApp/server.R
@@ -9,14 +9,67 @@
 
 library(shiny)
 
-predictText <- function(x){
-  if(x=="")
+# Load text prediction ngram model from csv file
+model <- read.csv("myModelStepFinal.0.csv")
+
+# Function to predict next word in sentence fragment passed as argument
+predictNextWord <- function(s)
+{
+  print("Calling prediction function...")
+  print(s)
+  # Since this will be reactive, do nothing if user hasn't yet typed anything
+  if(s == "")
   {
-    return("")
+    outList <- list()
+    outList[[1]] <- ""
+    return(outList)
   }
   
-  text <- paste(x, "test")
-  return(text)
+  print("Predicting for non-null sentence")
+  
+  # TODO: Apply same transformations to sentence as to training corpus
+  
+  # Split sentence into words 
+  tokenizedSentence = strsplit(s, " ")[[1]]
+  
+  # Separate the n-grams into different tables
+  modelBigrams    <- model[which(model$thirdWord=='^' & model$fourthWord=='^'),]
+  modelTrigrams   <- model[which(model$thirdWord!='^' & model$fourthWord=='^'),]
+  modelTetragrams <- model[which(model$fourthWord!='^'),]
+  
+  # Check if there is a known tetragram starting with the last three words of the sentence
+  tetragramCandidates <- modelTetragrams[which(modelTetragrams$firstWord==tokenizedSentence[1] & modelTetragrams$secondWord==tokenizedSentence[2] & modelTetragrams$thirdWord==tokenizedSentence[3]),]
+  trigramCandidates    <- modelTrigrams[which(modelTrigrams$firstWord==tokenizedSentence[2] & modelTrigrams$secondWord==tokenizedSentence[3] & modelTrigrams$thirdWord!="^"),]
+  bigramCandidates    <- modelBigrams[which(modelBigrams$firstWord==tokenizedSentence[3] & modelBigrams$secondWord!="^"),]
+  
+  if(nrow(tetragramCandidates) > 0)
+  {
+    topWord <- paste(s, tetragramCandidates[which.max(tetragramCandidates$cnt), "fourthWord"], sep = " ")
+    outList <- list()
+    outList[[1]] <- topWord
+    #outList[[2]] <- tetragramCandidates[, "fourthWord"]
+    return(outList)      
+  }
+  else if(nrow(trigramCandidates) > 0)
+  {
+    topWord <- paste(s, trigramCandidates[which.max(trigramCandidates$cnt), "thirdWord"], sep = " ")
+    outList <- list()
+    outList[[1]] <- topWord
+    return(outList)
+  }
+  else if(nrow(bigramCandidates) > 0)
+  {
+    topWord <- paste(s, bigramCandidates[which.max(bigramCandidates$cnt), "secondWord"], sep = " ")
+    outList <- list()
+    outList[[1]] <- topWord
+    return(outList)
+  }
+  else
+  {
+    outList <- list()
+    outList[[1]] <- "NO COMPLETION FOUND"
+    return(outList)
+  }
 }
 
 # Define server logic required to draw a histogram
@@ -33,8 +86,14 @@ shinyServer(function(input, output) {
     
   })
   
+  # Run prediction function, which returns a list
+  # The first item in the list is the prediction
+  # The second item is a dataframe with the most likely words to complete the sentence
+
+  getPrediction <- reactive({predictNextWord(input$inputSentence)})
+  
   output$outputSentence <- renderText({ 
-    predictText(input$inputSentence)
+    getPrediction()[[1]]
   })
   
 })
