42d219d8e5b73f754ecccf94daf62967ce0c44e5
jdokoop <orjuela.koop@gmail.com>
2016-09-24 10:49:25 -0600

    Cosmetic changes

--- TextPredictionApp/ui.R
+++ TextPredictionApp/ui.R
@@ -18,10 +18,11 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       h5("Start by typing a sentence in the box below. The prediction for the next word will appear below.
-         The graph on the right will show the top words most likely to complete your sentence."),
+       h5("Start by typing a sentence in the box below. The prediction for the next word will appear.
+         The graph on the right will show the top words most likely to complete your sentence. Please
+          allow 5 seconds for the model the load."),
        
-       h5(" "),
+       h5("__________________________________"),
     
        h5("Start Typing Here"),
       
