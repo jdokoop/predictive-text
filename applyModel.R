7037bcf2c08c578c3e1a5b566d478707cf2827d4
jdokoop <orjuela.koop@gmail.com>
2016-09-27 07:17:51 -0600

    Extend model to work with arbitrary number of input words

--- applyModel.R
+++ applyModel.R
@@ -3,6 +3,11 @@ applyModel <- function(sentence)
   # Read model from file
   model <- read.csv("myModelStepFinal.0.csv")
   
+  # Separate the n-grams into different tables
+  modelBigrams    <- model[which(model$thirdWord=='^' & model$fourthWord=='^'),]
+  modelTrigrams   <- model[which(model$thirdWord!='^' & model$fourthWord=='^'),]
+  modelTetragrams <- model[which(model$fourthWord!='^'),]
+  
   # PREDICTION FUNCTION BASED ON MODEL
   predictNextWord <- function(s)
   {
@@ -10,40 +15,41 @@ applyModel <- function(sentence)
     
     # Split sentence into words 
     tokenizedSentence = strsplit(s, " ")[[1]]
-  
-    # Separate the n-grams into different tables
-    modelBigrams    <- model[which(model$thirdWord=='^' & model$fourthWord=='^'),]
-    modelTrigrams   <- model[which(model$thirdWord!='^' & model$fourthWord=='^'),]
-    modelTetragrams <- model[which(model$fourthWord!='^'),]
-
-    # Check if there is a known tetragram starting with the last three words of the sentence
-    tetragramCandidates <- modelTetragrams[which(modelTetragrams$firstWord==tokenizedSentence[1] & modelTetragrams$secondWord==tokenizedSentence[2] & modelTetragrams$thirdWord==tokenizedSentence[3]),]
-    trigramCandidates   <- modelTrigrams[which(modelTrigrams$firstWord==tokenizedSentence[2] & modelTrigrams$secondWord==tokenizedSentence[3] & modelTrigrams$thirdWord!="^"),]
-    bigramCandidates    <- modelBigrams[which(modelBigrams$firstWord==tokenizedSentence[3] & modelBigrams$secondWord!="^"),]
+    sentenceLength    = length(tokenizedSentence)
     
-    if(nrow(tetragramCandidates) > 0)
+    # Look for ngrams in the model which complete the sentence using a backoff scheme
+    tetragramCandidates <- modelTetragrams[which(modelTetragrams$firstWord==tokenizedSentence[sentenceLength - 2] & modelTetragrams$secondWord==tokenizedSentence[sentenceLength - 1] & modelTetragrams$thirdWord==tokenizedSentence[sentenceLength]),]
+    trigramCandidates   <- modelTrigrams[which(modelTrigrams$firstWord==tokenizedSentence[sentenceLength - 1] & modelTrigrams$secondWord==tokenizedSentence[sentenceLength] & modelTrigrams$thirdWord!="^"),]
+    bigramCandidates    <- modelBigrams[which(modelBigrams$firstWord==tokenizedSentence[sentenceLength] & modelBigrams$secondWord!="^"),]
+    
+    # Sort the resulting candidate completions by their likelihood
+    tetragramCandidates <- tetragramCandidates[order(-tetragramCandidates$cnt), ]
+    trigramCandidates   <- trigramCandidates[order(-trigramCandidates$cnt), ]
+    bigramCandidates    <- bigramCandidates[order(-bigramCandidates$cnt), ]
+    
+    # Use backoff scheme depending on sentence length
+    # This way we have a prediction regardless of how many words the user types
+    if(nrow(tetragramCandidates) > 0 & sentenceLength > 2)
     {
-      print("Using tetragrams")
-      result <- paste(sentence, tetragramCandidates[which.max(tetragramCandidates$cnt), "fourthWord"], sep = " ")
-      print(result)      
+      topWord <- paste(s, tetragramCandidates[which.max(tetragramCandidates$cnt), c("fourthWord")], sep = " ")
+      return(topWord)      
     }
-    else if(nrow(trigramCandidates) > 0)
+    else if(nrow(trigramCandidates) > 0 & sentenceLength > 1)
     {
-      print("Using trigrams")
-      result <- paste(sentence, trigramCandidates[which.max(trigramCandidates$cnt), "thirdWord"], sep = " ")
-      print(result)
+      topWord <- paste(s, trigramCandidates[which.max(trigramCandidates$cnt), c("thirdWord")], sep = " ")
+      return(topWord)      
     }
     else if(nrow(bigramCandidates) > 0)
     {
-      print("Using bigrams")
-      result <- paste(sentence, bigramCandidates[which.max(bigramCandidates$cnt), "secondWord"], sep = " ")
-      print(result)
+      topWord <- paste(s, bigramCandidates[which.max(bigramCandidates$cnt), c("secondWord")], sep = " ")
+      return(topWord)      
     }
     else
     {
-      print("NO COMPLETION FOUND")
+      return("---")
     }
   }
   
-  predictNextWord(sentence)
+  prediction <- predictNextWord(sentence)
+  print(prediction)
 }
