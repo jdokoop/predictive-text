3ad3be87df35fd3e31a83e62bbe423e5beb94139
jdokoop <orjuela.koop@gmail.com>
2016-09-30 19:45:45 -0600

    First commit

---
+++ mergeModel.R
@@ -0,0 +1,116 @@
+mergeModel <- function()
+{
+  library(dplyr)
+  library(plyr)
+  
+  combineModels <- function(model0, model1)
+  {
+    # Do a full outer join of the two data frames
+    model <- merge(model0, model1, by=c("firstWord", "secondWord", "thirdWord", "fourthWord"), all = TRUE)
+    
+    # Replace NAs by 0
+    model[is.na(model)] <- 0
+    
+    # Drop unnecessary columns produced in merging
+    model$cnt <- model$cnt.x + model$cnt.y
+    model <- subset(model, select = -c(X.x, X.y, cnt.x, cnt.y) )
+    
+    # Select only ngrams with frequency > 1
+    model <- model[which(model$cnt > 1),]
+    
+    return(model)    
+  }
+
+  numFiles <- 0  
+  for(i in c(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39))
+  {
+    model0 <- read.csv(sprintf("ModelFragments/myModel.%i.csv",i-1))
+    print(sprintf("Read %i", i-1))
+    model1 <- read.csv(sprintf("ModelFragments/myModel.%i.csv",i))
+    print(sprintf("Read %i", i))
+  
+    model <- combineModels(model0, model1)
+    rm(model0)
+    rm(model1)
+    print("Combined models...")
+  
+    write.csv(model, file=sprintf("MergedModels/myModelStepA.%i.csv", numFiles))
+    numFiles <- numFiles + 1
+  }
+  
+  numFiles <- 0
+  for(i in c(1, 3, 5, 7, 9, 11, 13, 15, 17, 19))
+  {
+    model0 <- read.csv(sprintf("MergedModels/myModelStepA.%i.csv",i-1))
+    print(sprintf("Read %i", i-1))
+    model1 <- read.csv(sprintf("MergedModels/myModelStepA.%i.csv",i))
+    print(sprintf("Read %i", i))
+    
+    model <- combineModels(model0, model1)
+    rm(model0)
+    rm(model1)
+    print("Combined models...")
+    
+    write.csv(model, file=sprintf("MergedModels/myModelStepB.%i.csv", numFiles))
+    numFiles <- numFiles + 1
+  }
+  
+  numFiles <- 0
+  for(i in c(1, 3, 5, 7, 9))
+  {
+    model0 <- read.csv(sprintf("MergedModels/myModelStepB.%i.csv",i-1))
+    print(sprintf("Read %i", i-1))
+    model1 <- read.csv(sprintf("MergedModels/myModelStepB.%i.csv",i))
+    print(sprintf("Read %i", i))
+    
+    model <- combineModels(model0, model1)
+    rm(model0)
+    rm(model1)
+    print("Combined models...")
+    
+    write.csv(model, file=sprintf("MergedModels/myModelStepC.%i.csv", numFiles))
+    numFiles <- numFiles + 1
+  }
+  
+  numFiles <- 0
+
+  model0 <- read.csv(sprintf("MergedModels/myModelStepC.%i.csv",0))
+  print(sprintf("Read %i", 0))
+  model1 <- read.csv(sprintf("MergedModels/myModelStepC.%i.csv",1))
+  print(sprintf("Read %i", 1))
+    
+  model <- combineModels(model0, model1)
+  rm(model0)
+  rm(model1)
+  print("Combined models...")
+    
+  write.csv(model, file=sprintf("MergedModels/myModelStepD.%i.csv", 0))
+  
+  numFiles <- 0
+  
+  model0 <- read.csv(sprintf("MergedModels/myModelStepC.%i.csv",2))
+  print(sprintf("Read %i", 2))
+  model1 <- read.csv(sprintf("MergedModels/myModelStepC.%i.csv",3))
+  print(sprintf("Read %i", 3))
+  
+  model <- combineModels(model0, model1)
+  rm(model0)
+  rm(model1)
+  print("Combined models...")
+  
+  write.csv(model, file=sprintf("MergedModels/myModelStepD.%i.csv", 1))
+  
+  numFiles <- 0
+  
+  model0 <- read.csv(sprintf("MergedModels/myModelStepD.%i.csv",0))
+  print(sprintf("Read %i", 0))
+  model1 <- read.csv(sprintf("MergedModels/myModelStepD.%i.csv",1))
+  print(sprintf("Read %i", 1))
+  
+  model <- combineModels(model0, model1)
+  rm(model0)
+  rm(model1)
+  print("Combined models...")
+  
+  write.csv(model, file=sprintf("MergedModels/myModelStepFinal.%i.csv", 0))
+}
